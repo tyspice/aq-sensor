56d426050d7ecea91d01242757dda085f4f721a9
tyspice <tyner@tyspice.dev>
2024-04-07 12:36:49 -0700

    add WifiHelper documentation

--- src/WifiHelper.h
+++ src/WifiHelper.h
@@ -1,13 +1,26 @@
 #pragma once
 
+/**
+ * The WifiHelper Singleton class provides a helper for managing WiFi connections.
+ */
 class WifiHelper {
 private:
-  WifiHelper();
-  bool _init();
-  bool m_initialized;
+  WifiHelper(); /**< Private constructor to prevent instantiation. */
+  bool _init(); /**< Initialize the WiFi helper. */
+  bool m_initialized; /**< Flag indicating whether the WiFi helper is initialized. */
 
 public:
-  WifiHelper(const WifiHelper&) = delete;
+  WifiHelper(const WifiHelper&) = delete; /**< Deleted copy constructor to prevent copying. */
+  
+  /**
+   * Initializes the WiFi helper.
+   * @return True if initialization is successful, false otherwise.
+   */
   static bool init();
+  
+  /**
+   * Retrieves the singleton instance of the WiFi helper.
+   * @return Reference to the singleton instance of the WiFi helper.
+   */
   static WifiHelper& getInstance();
 };
