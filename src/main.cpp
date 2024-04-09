6035f979c0c4c034bf6da550270f02ced979943c
tyspice <tyner@tyspice.dev>
2024-04-08 22:19:58 -0700

    add publisher and statusMonitor tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,49 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include <iostream>
+#include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 #include "WifiHelper.h"
 #include "MQTTClient.h"
 using std::string;
 
-void main_task(void *params) {
+#define BOARD_LED 25
+
+void publisher(void* params) {
+  MQTTClient* mqttClient = static_cast<MQTTClient*>(params);
+  for ( ;; ) {
+    err_t err = mqttClient->publish(string("Hello"), string("World"));
+    vTaskDelay(5000);
+  }
+}
+
+void statusMonitor(void* params) {
+  MQTTClient* mqttClient = static_cast<MQTTClient*>(params);
+  // Display WIFI and MQTT connection statuses via onboard LED
+  for ( ;; ) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    vTaskDelay(500);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    vTaskDelay(500);
+  }
+}
+
+void mainTask(void *params) {
   bool wifiReady = WifiHelper::init();
   MQTTClient mqttClient;
 
-  while(1) {
-    err_t err = mqttClient.publish(string("Hello"), string("World"));
-    vTaskDelay(1000);
-  }
+  TaskHandle_t publisherTaskHandle;
+  TaskHandle_t statusMonitorTaskHandle;
+
+  xTaskCreate(publisher, "publisher", configMINIMAL_STACK_SIZE, &mqttClient, tskIDLE_PRIORITY, &publisherTaskHandle);
+  xTaskCreate(statusMonitor, "statusMonitor", configMINIMAL_STACK_SIZE, &mqttClient, tskIDLE_PRIORITY, &statusMonitorTaskHandle);
+
+  while(1) { vTaskDelay(1000); }
 }
 
 int main() {
   stdio_init_all();
   TaskHandle_t task;
-  xTaskCreate(main_task, "TestMainThread", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, &task);
+  xTaskCreate(mainTask, "mainTask", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, &task);
   vTaskStartScheduler();
 }
